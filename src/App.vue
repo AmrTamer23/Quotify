16475ac120e1e8b054380c6aaca5fb4c198eedc7
AmrTamer23 <amr12300062@cs.modern-academy.edu.eg>
2024-02-11 18:40:31 +0200

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <main class="px-10 lg:px-20 py-20">
+    <h1 class="mx-auto font-bold">
+      Quotify
+    </h1>
+
+  </main>
+</template>
+
