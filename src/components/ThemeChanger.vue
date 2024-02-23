b23c56093c6ca5824a47bf63e1996ab1308eebd5
AmrTamer23 <amr12300062@cs.modern-academy.edu.eg>
2024-02-23 22:33:33 +0200

    Feat: added solarized theme and enhanced theme changer

--- src/components/ThemeChanger.vue
+++ src/components/ThemeChanger.vue
@@ -8,6 +8,24 @@ import {
 import { useThemeStore, setTheme } from '@/stores/theme'
 const themeStore = useThemeStore()
 setTheme(themeStore.theme)
+const themesList: {
+  name: string
+  color: Theme
+}[]
+  = [
+    {
+      name: 'Default',
+      color: 'default'
+    },
+    {
+      name: 'Vesper',
+      color: 'vesper'
+    },
+    {
+      name: 'Solarized',
+      color: 'solarized'
+    }
+  ]
 </script>
 
 <template>
@@ -17,18 +35,13 @@ setTheme(themeStore.theme)
         <v-icon name="bi-palette" scale="2" />
       </button>
     </DropdownMenuTrigger>
-    <DropdownMenuContent class="w-56 mx-4 bg-card-base p-0">
-      <div class="flex flex-col gap-2 p-4">
-        <button class="p-2 rounded-md text-button-themeSwitcher" name="default" @click="themeStore.change('default')"
-          @mouseenter="themeStore.changeOnHover('default')" @mouseleave="themeStore.changeOnHoverOut()">
-          <span>
-            Default
-          </span>
-        </button>
-        <button class="p-2 rounded-md text-button-themeSwitcher" name="vesper" @click="themeStore.change('vesper')"
-          @mouseenter="themeStore.changeOnHover('vesper')" @mouseleave="themeStore.changeOnHoverOut()">
-          <span>
-            Vesper
+    <DropdownMenuContent class="w-56 mx-4 bg-card-base p-0 ">
+      <div class="flex flex-col divide-y-2">
+        <button class="p-3 text-button-themeSwitcher my-0 rounded-none" v-for="theme in themesList" name="theme.name"
+          @click="themeStore.change(theme.color)" @mouseenter="themeStore.changeOnHover(theme.color)"
+          @mouseleave="themeStore.changeOnHoverOut()">
+          <span class="text-base">
+            {{ theme.name }}
           </span>
         </button>
       </div>
